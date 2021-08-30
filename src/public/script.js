41355cf4e27f8cad9f52288b5a4306fd5931561d
Junaid Khan <junaid.khan.17@mountblue.tech>
2021-08-30 14:22:10 +0530

    modified 2nd questions visualization

--- src/public/script.js
+++ src/public/script.js
@@ -20,9 +20,9 @@ fetchAll(urls);
 
 function visualizeData(data) {
   visMatchesPerYear(data[0]);
-  visMatchesWonPerYear(data[1]);
   visExtraRunPerTeamIn2016(data[2]);
   visTop10EconomicalBowlersIn2015(data[3]);
+  visMatchesWonPerYear(data[1]);
   return;
 }
 
@@ -61,61 +61,127 @@ function visMatchesPerYear(matchesPerYear) {
 }
 
 function visMatchesWonPerYear(matchesWonPerYear) {
-  let won = [];
-  let year = [];
-  let teamName = [];
-  for (let key in matchesWonPerYear) {
-    year.push(key);
-  }
-  let arr = Object.values(matchesWonPerYear);
-  console.log(arr);
-  for (let i = 0; i < arr.length; i++) {
-    for (let val in arr[i]) {
-      if (!(val in teamName)) {
-        teamName.push(val);
-      }
-      won.push([val, arr[i][val]]);
-    }
-  }
+  let years = [];
+  let CSK = [],
+    DD = [],
+    RCB = [],
+    KKR = [],
+    RR = [],
+    KXIP = [],
+    DC = [],
+    MI = [],
+    PW = [],
+    KTK = [],
+    SRH = [],
+    GR = [],
+    RPS = [];
 
-  Highcharts.chart("matchesWonPerYear", {
+  Object.entries(matchesWonPerYear).forEach((element) => {
+    const [key, value] = element;
+    years.push(key);
+    const index = parseInt(key) - 2008;
+    Object.entries(value).forEach((data) => {
+      const [keys, values] = data;
+      console.log
+      switch (keys) {
+        case 'Chennai Super Kings':
+          CSK[index] = values;
+          break;
+        case 'Delhi Daredevils':
+          DD[index] = values;
+          break;
+        case 'Royal Challengers Bangalore':
+          RCB[index] = values;
+          break;
+        case 'Kolkata Knight Riders':
+          KKR[index] = values;
+          break;
+        case 'Rajasthan Royals':
+          RR[index] = values;
+          break;
+        case 'Kings XI Punjab':
+          KXIP[index] = values;
+          break;
+        case 'Deccan Chargers':
+          DC[index] = values;
+          break;
+        case 'Mumbai Indians':
+          MI[index] = values;
+          break;
+        case 'Pune Warriors':
+          PW[index] = values;
+          break;
+        case 'Kochi Tuskers Kerala':
+          KTK[index] = values;
+          break;
+        case 'Sunrisers Hyderabad':
+          SRH[index] = values;
+          break;
+        case 'Gujarat Lions':
+          GR[index] = values;
+          break;
+        default: RPS[index] = values;
+        break;
+      }
+    });
+  });
+
+  Highcharts.chart('matchesWonPerYear', {
     chart: {
-      type: "bar",
+        type: 'column'
     },
     title: {
-      text: "Stacked bar chart",
+        text: 'Matches Won Per Year'
     },
     xAxis: {
-      categories: teamName,
+        categories: years
     },
     yAxis: {
-      min: 0,
-      title: {
-        text: "Total fruit consumption",
-      },
+        min: 0,
+        title: {
+            text: 'Number of Matches Won'
+        }
     },
-    legend: {
-      reversed: true,
-    },
-    plotOptions: {
-      series: {
-        stacking: "normal",
-      },
-    },
-    series: [
-      {
-        name: year[0],
-        data: won,
-      },
-      {
-        name: year[1],
-        data: won,
-      },
-      {
-        name: year[2],
-        data: won,
-      },
-    ],
+    series: [{
+      name: 'Chennai Super Kings',
+      data: CSK
+    }, {
+      name: 'Delhi Daredevils',
+      data: DD
+    }, {
+      name: 'Royal Challengers Bangalore',
+      data: RCB
+    }, {
+      name: 'Kolkata Knight Riders',
+      data: KKR
+    }, {
+      name: 'Rajasthan Royals',
+      data: RR
+    }, {
+      name: 'Kings XI Punjab',
+      data: KXIP
+    }, {
+      name: 'Deccan Chargers',
+      data: DC
+    }, {
+      name: 'Mumbai Indians',
+      data: MI
+    }, {
+      name: 'Pune Warriors',
+      data: PW
+    }, {
+      name: 'Kochi Tuskers Kerala',
+      data: KTK
+    }, {
+      name: 'Sunrisers Hyderabad',
+      data: SRH
+    }, {
+      name: 'Gujarat Lions',
+      data: GR
+    }, {
+      name: 'Rising Pune Supergiants',
+      data: RPS
+    }]
   });
 }
 
