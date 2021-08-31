21b9cde62c7d64c27156aeb338eda98779162344
Junaid Khan <junaid.khan.17@mountblue.tech>
2021-08-31 15:31:34 +0530

    initial commit

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,90 +1,112 @@
 module.exports.matchesPerYear = (matches) => {
-  const countPlayed = {};
+  let matchesArr = [];
   for (let match of matches) {
-    const season = match.season;
-    if (countPlayed[season]) {
-      countPlayed[season] += 1;
-    } else {
-      countPlayed[season] = 1;
-    }
+    matchesArr.push(match);
   }
-  return countPlayed;
+  let result = {};
+  matchesArr
+    .map((obj) => obj["season"])
+    .forEach((element) => {
+      if (element in result) {
+        result[element] += 1;
+      } else {
+        result[element] = 1;
+      }
+    });
+  return result;
 };
 
 module.exports.matchesWonPerYear = (matches) => {
+  let matchesArr = [];
+  for (let match of matches) {
+    matchesArr.push(match);
+  }
   let countWon = {};
   let result = {};
-  for (let match of matches) {
-    const winner = match.winner;
-    const season = match.season;
-    if (season in result) {
-      if (winner in countWon) countWon[winner] += 1;
-      else countWon[winner] = 1;
 
-      result[season] = countWon;
+  matchesArr.forEach((element) => {
+    if (element.season in result) {
+      if (element.winner in countWon) countWon[element.winner] += 1;
+      else countWon[element.winner] = 1;
+
+      result[element.season] = countWon;
     } else {
       countWon = {};
-      result[season] = countWon;
+      result[element.season] = countWon;
     }
-  }
+  });
 
   return result;
 };
 
 module.exports.extraRunPerTeamIn2016 = (matches, deliveries) => {
+  let matchesArr = [],
+    deliveriesArr = [];
+
+  for (let match of matches) {
+    matchesArr.push(match);
+  }
+  for (let delivery of deliveries) {
+    deliveriesArr.push(delivery);
+  }
+
   let result = {};
   let matchId = 0;
-  for (let match of matches) {
-    const season = match.season;
-    const id = match.id;
-    if (season == 2016) {
-      matchId = id;
+
+  matchesArr.forEach((element) => {
+    if (element.season == 2016) {
+      matchId = element.id;
     }
 
-    for (let delivery of deliveries) {
-      const match_id = delivery.match_id;
-      const bowling_team = delivery.bowling_team;
-      const extra_runs = delivery.extra_runs;
-      if (match_id == matchId) {
-        if (bowling_team in result) {
-          result[bowling_team] += Number(extra_runs);
-        } else {
-          result[bowling_team] = Number(extra_runs);
-        }
+    deliveriesArr.forEach((delivery) => {
+      if (delivery.match_id == matchId) {
+        if (delivery.bowling_team in result)
+          result[delivery.bowling_team] += Number(delivery.extra_runs);
+        else result[delivery.bowling_team] = Number(delivery.extra_runs);
       }
-    }
-  }
+    });
+  });
+
   return result;
 };
 
 module.exports.top10EconomicalBowlersIn2015 = (matches, deliveries) => {
-  let matchId = 0;
-  let economy = {};
+  let matchesArr = [],
+    deliveriesArr = [];
+
   for (let match of matches) {
-    const season = match.season;
-    const id = match.id;
-    if (season == 2015) {
-      matchId = id;
+    matchesArr.push(match);
+  }
+  for (let delivery of deliveries) {
+    deliveriesArr.push(delivery);
+  }
+
+  let economy = {};
+  let matchId = 0;
+
+  matchesArr.forEach((element) => {
+    if (element.season == 2016) {
+      matchId = element.id;
     }
 
-    for (let delivery of deliveries) {
-      const match_id = delivery.match_id;
-      const bowler = delivery.bowler;
-      const over = delivery.over;
-      const total_runs = delivery.total_runs;
-      if (match_id == matchId) {
-        if (bowler in economy) {
-          economy[bowler] += Number(total_runs / over);
-        } else {
-          economy[bowler] = Number(total_runs / over);
-        }
+    deliveriesArr.forEach((delivery) => {
+      if (delivery.match_id == matchId) {
+        if (delivery.bowler in economy)
+          economy[delivery.bowler] += Number(
+            (delivery.total_runs / delivery.over).toFixed(2)
+          );
+        else
+          economy[delivery.bowler] = Number(
+            (delivery.total_runs / delivery.over).toFixed(2)
+          );
       }
-    }
-  }
+    });
+  });
 
   let economyInSortedOrder = Object.entries(economy).sort(
     (a, b) => a[1] - b[1]
   );
-  let result = economyInSortedOrder.slice(0, 10);
+  let top10 = economyInSortedOrder.slice(0, 10);
+  let result = Object.fromEntries(top10)
   return result;
 };
